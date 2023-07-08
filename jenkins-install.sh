10185dc88e01d07d45faf13b76b85433e19ddc34
Showmik Bose <officialshowmik@gmail.com>
2023-07-08 19:00:33 +0530

    Update jenkins-install.sh
    Removed old rpm keys

--- jenkins-install.sh
+++ jenkins-install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 sudo yum update –y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+#Removing old RPM Keys
+#sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 #sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum upgrade -y
 sudo amazon-linux-extras install java-openjdk11 -y
